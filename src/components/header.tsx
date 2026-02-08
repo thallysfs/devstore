a34f09e6b28979b54ce3c8c9df224871fd860025
thallysfs <thallys@hotmail.com>
2026-02-08 19:33:03 -0300

    feat: create home component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import Link from "next/link"
-import { Search } from 'lucide-react'
+import { Search, ShoppingBag } from 'lucide-react'
+import Image from "next/image"
 
 function Header() {
   return (
@@ -10,12 +13,30 @@ function Header() {
         </Link>
 
         <form className="flex w-[320] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search />
+          <Search className="w-5 h-5 text-zinc-500" />
+
+          <input type="text" placeholder="Buscar produto" className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500" />
         </form>
       </div>
 
       <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <ShoppingBag className="h-4 w-4" />
+          <span className="text-sm">Cart (0)</span>
+        </div>
 
+        <div className="w-px h-4 bg-zinc-700" />
+
+        <Link href="/" className="flex items-center gap-2 hover:underline">
+          <span className="text-sm">Account</span>
+          <Image
+            src="https://github.com/thallysfs.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt=""
+          />
+        </Link>
       </div>
     </div>
   )
