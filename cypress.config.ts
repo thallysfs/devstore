0039f6a3fa62745385dea00e70bb83d13e9d9f20
thallysfs <thallys@hotmail.com>
2026-02-12 21:44:20 -0300

    feat: add cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,39 @@
+describe('add product to cart', () => {
+    beforeEach(() => {
+    cy.visit('/')
+  })
+  it('should be able to navigate ti the product page and add it to the cart', () => {
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+    it('should not count duplicated product on cart', () => {
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+    cy.contains('Adicionar ao carrinho').click()
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+
+    cy.searchByQuery('moletom')
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+
+    cy.contains('Adicionar ao carrinho').click()
+
+    cy.contains('Cart (1)').should('exist')
+
+  })
+}) 
